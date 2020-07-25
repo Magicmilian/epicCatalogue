3f1826f1cd359c85477c7490eac5bd1df729c0a1
Maximiliano Diaz Huerta <diazhuerta93@gmail.com>
2020-07-25 07:11:26 -0300

    Se finalizo por completo la logica de la pagina Admin

--- src/js/admin.js
+++ src/js/admin.js
@@ -57,6 +57,11 @@ window.destacador = function (codigo) {
         if (listaProductos[i].codigo == codigo) {
             if (listaProductos[i].destacado == false) {
                 listaProductos[i].destacado = true;
+                for (let i in listaProductos) {
+                    if ((listaProductos[i]).codigo != codigo) {
+                        listaProductos[i].destacado = false;
+                    }
+                }
                 localStorage.setItem("productoKey", JSON.stringify(listaProductos));
             }
             else {
@@ -76,9 +81,9 @@ function dibujarFilas(productosLS) {
         if (productosLS[i].publicado == false && productosLS[i].destacado == false) {
             codigoHTML = `<tr>
         <td scope="row">${productosLS[i].codigo}</th>
-        <td>${productosLS[i].nombre}</td>
-        <td>${productosLS[i].categoria}</td>
-        <td class="descripcion">${productosLS[i].descripcion}</td>
+        <td class="tablaNombre">${productosLS[i].nombre}</td>
+        <td class="tablaCategoria">${productosLS[i].categoria}</td>
+        <td class="tablaDescripcion">${productosLS[i].descripcion}</td>
         <td>
             <label class="checkContainer">
                 <input id="${productosLS[i].codigo}" onchange="publicador(this.id)" type="checkbox">
@@ -86,8 +91,8 @@ function dibujarFilas(productosLS) {
             </label>
         </td>
         <td class="opciones">
-            <button class="btnSizeCustom"><i class="far fa-trash-alt fa-1x"></i></button>
-            <button><i class="far fa-edit fa-1x"></i></button>
+            <button onclick="eliminarProducto(${productosLS[i].codigo})" class="btnSizeCustom"><i class="far fa-trash-alt fa-1x"></i></button>
+            <button onclick="abrirModificarProducto(${productosLS[i].codigo})"><i class="far fa-edit fa-1x"></i></button>
             <button onclick="destacador(${productosLS[i].codigo})"><i class="far fa-star fa-1x"></i></button>
         </td>
     </tr>`
@@ -96,9 +101,9 @@ function dibujarFilas(productosLS) {
         else if (productosLS[i].publicado == true && productosLS[i].destacado == false) {
             codigoHTML = `<tr>
         <td scope="row">${productosLS[i].codigo}</th>
-        <td>${productosLS[i].nombre}</td>
-        <td>${productosLS[i].categoria}</td>
-        <td class="descripcion">${productosLS[i].descripcion}</td>
+        <td class="tablaNombre">${productosLS[i].nombre}</td>
+        <td class="tablaCategoria">${productosLS[i].categoria}</td>
+        <td class="tablaDescripcion">${productosLS[i].descripcion}</td>
         <td>
             <label class="checkContainer">
                 <input id="${productosLS[i].codigo}" onchange="publicador(this.id)" type="checkbox" checked>
@@ -106,8 +111,8 @@ function dibujarFilas(productosLS) {
             </label>
         </td>
         <td class="opciones">
-            <button class="btnSizeCustom"><i class="far fa-trash-alt fa-1x"></i></button>
-            <button><i class="far fa-edit fa-1x"></i></button>
+            <button onclick="eliminarProducto(${productosLS[i].codigo})" class="btnSizeCustom"><i class="far fa-trash-alt fa-1x"></i></button>
+            <button onclick="abrirModificarProducto(${productosLS[i].codigo})"><i class="far fa-edit fa-1x"></i></button>
             <button onclick="destacador(${productosLS[i].codigo})"><i class="far fa-star fa-1x"></i></button>
         </td>
     </tr>`
@@ -116,9 +121,9 @@ function dibujarFilas(productosLS) {
         else if (productosLS[i].publicado == false && productosLS[i].destacado == true) {
             codigoHTML = `<tr>
         <td scope="row">${productosLS[i].codigo}</th>
-        <td>${productosLS[i].nombre}</td>
-        <td>${productosLS[i].categoria}</td>
-        <td class="descripcion">${productosLS[i].descripcion}</td>
+        <td class="tablaNombre">${productosLS[i].nombre}</td>
+        <td class="tablaCategoria">${productosLS[i].categoria}</td>
+        <td class="tablaDescripcion">${productosLS[i].descripcion}</td>
         <td>
             <label class="checkContainer">
                 <input id="${productosLS[i].codigo}" onchange="publicador(this.id)" type="checkbox">
@@ -126,8 +131,8 @@ function dibujarFilas(productosLS) {
             </label>
         </td>
         <td class="opciones">
-            <button class="btnSizeCustom"><i class="far fa-trash-alt fa-1x"></i></button>
-            <button><i class="far fa-edit fa-1x"></i></button>
+            <button onclick="eliminarProducto(${productosLS[i].codigo})" class="btnSizeCustom"><i class="far fa-trash-alt fa-1x"></i></button>
+            <button onclick="abrirModificarProducto(${productosLS[i].codigo})"><i class="far fa-edit fa-1x"></i></button>
             <button onclick="destacador(${productosLS[i].codigo})"><i class="fas fa-star fa-1x"></i></button>
         </td>
     </tr>`
@@ -136,9 +141,9 @@ function dibujarFilas(productosLS) {
         else if (productosLS[i].publicado == true && productosLS[i].destacado == true) {
             codigoHTML = `<tr>
         <td scope="row">${productosLS[i].codigo}</th>
-        <td>${productosLS[i].nombre}</td>
-        <td>${productosLS[i].categoria}</td>
-        <td class="descripcion">${productosLS[i].descripcion}</td>
+        <td class="tablaNombre">${productosLS[i].nombre}</td>
+        <td class="tablaCategoria">${productosLS[i].categoria}</td>
+        <td class="tablaDescripcion">${productosLS[i].descripcion}</td>
         <td>
             <label class="checkContainer">
                 <input id="${productosLS[i].codigo}" onchange="publicador(this.id)" type="checkbox" checked>
@@ -146,8 +151,8 @@ function dibujarFilas(productosLS) {
             </label>
         </td>
         <td class="opciones">
-            <button class="btnSizeCustom"><i class="far fa-trash-alt fa-1x"></i></button>
-            <button><i class="far fa-edit fa-1x"></i></button>
+            <button onclick="eliminarProducto(${productosLS[i].codigo})" class="btnSizeCustom"><i class="far fa-trash-alt fa-1x"></i></button>
+            <button onclick="abrirModificarProducto(${productosLS[i].codigo})"><i class="far fa-edit fa-1x"></i></button>
             <button onclick="destacador(${productosLS[i].codigo})"><i class="fas fa-star fa-1x"></i></button>
         </td>
     </tr>`
@@ -175,13 +180,14 @@ window.limpiarForm = function () {
     let imagen = document.getElementById("imagen");
     let iframeurl = document.getElementById("iframeurl");
     let formProducto = document.getElementById("formProducto");
+    document.getElementById("codigo").removeAttribute("disabled");
     formProducto.reset();
-    codigo.className = "form-control"
-    nombre.className = "form-control"
-    categoria.className = "form-control"
-    descripcion.className = "form-control"
-    imagen.className = "form-control"
-    iframeurl.className = "form-control"
+    codigo.className = "form-control";
+    nombre.className = "form-control";
+    categoria.className = "form-control";
+    descripcion.className = "form-control";
+    imagen.className = "form-control";
+    iframeurl.className = "form-control";
     productoExistente = false;
 }
 
@@ -221,9 +227,133 @@ function agregarProducto() {
         campoRequerido(categoria);
         campoRequerido(descripcion);
         campoRequerido(imagen);
+        validarUrl(iframeurl);
         document.getElementById("validacionGeneral").className = "is-invalid";
     }
 }
+
+//FUNCIONALIDAD PARA ABRIR EL MODAL CON LOS DATOS CARGADOS
+window.abrirModificarProducto = function (codigo) {
+    //Buscar en el arreglo el objeto que tiene el codigo recibido por parametro
+    let objetoEncontrado = listaProductos.find(function (producto) {
+        return producto.codigo == codigo;
+    })
+    //Asignar al modal formulario los valores del objeto encontrado
+    document.getElementById("codigo").value = objetoEncontrado.codigo;
+    document.getElementById("codigo").setAttribute("disabled", "");
+    document.getElementById("nombre").value = objetoEncontrado.nombre;
+    document.getElementById("categoria").value = objetoEncontrado.categoria;
+    document.getElementById("descripcion").value = objetoEncontrado.descripcion;
+    document.getElementById("imagen").value = objetoEncontrado.imagen;
+    document.getElementById("iframeurl").value = objetoEncontrado.iframeurl;
+    //Mostrar la ventana modal
+    let ventanaModal = document.getElementById("modalProducto");
+    $(ventanaModal).modal("show");
+    productoExistente = true;
+}
+
+//FUNCIONALIDAD BOTON ENVIAR - PRODUCTO MODIFICADO
+window.guardarProductoModificado = function () {
+    //Traigo los valores del formulario y los defino como variables
+    let codigo = document.getElementById("codigo");
+    let nombre = document.getElementById("nombre");
+    let categoria = document.getElementById("categoria");
+    let descripcion = document.getElementById("descripcion");
+    let imagen = document.getElementById("imagen");
+    let iframeurl = document.getElementById("iframeurl");
+    //Valido el formulario de manera general
+    if (campoRequerido(nombre) && campoRequerido(categoria) && campoRequerido(descripcion) && campoRequerido(imagen) && validarUrl(iframeurl)) {
+        //Buscar el producto que estaba modificando en el arreglo y actualizar los valores
+        for (let i in listaProductos) {
+            if (listaProductos[i].codigo == codigo.value) {
+                listaProductos[i].nombre = nombre.value;
+                listaProductos[i].categoria = categoria.value;
+                listaProductos[i].descripcion = descripcion.value;
+                listaProductos[i].imagen = imagen.value;
+                listaProductos[i].iframeurl = iframeurl.value;
+            }
+            //Actualizar local Storage y dibujamos la tabla
+            localStorage.setItem("productoKey", JSON.stringify(listaProductos));
+            leerDatos();
+            limpiarForm();
+            let ventanaModal = document.getElementById("modalProducto");
+            $(ventanaModal).modal("hide");
+        }
+    } else {
+        campoRequerido(nombre);
+        campoRequerido(categoria);
+        campoRequerido(descripcion);
+        campoRequerido(imagen);
+        validarUrl(iframeurl);
+        document.getElementById("validacionGeneral").className = "is-invalid";
+    }
+}
+
+//FUNCIONALIDAD BOTON ELIMINAR
+window.eliminarProducto = function (codigo) {
+    let productosFiltrados = listaProductos.filter(function (producto) {
+        return producto.codigo != codigo;
+    });
+    localStorage.setItem("productoKey", JSON.stringify(productosFiltrados));
+    listaProductos = productosFiltrados;
+    leerDatos();
+}
+
+//FUNCIONALIDAD PARA SORTEAR LA TABLA
+window.sortTable = function(n) {
+    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("tablaProdAll");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
 
 //VALIDACION DE FORMULARIO
 window.validarCodigo = function (codigo) {
