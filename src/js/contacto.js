92cc901f9c39b0694c1cd13e71d24497b6fa8733
juan <juandami1982@gmail.com>
2020-07-26 17:42:02 -0300

    finalizado el html de contacto

--- src/js/contacto.js
+++ src/js/contacto.js
@@ -1,5 +1,5 @@
 function requerido(input) {
-    // let elemento = document.getElementById("nombre");
+
     if (input.value != "") {
         //   el input tiene texto
         input.className = "form-control is-valid";
@@ -12,6 +12,32 @@ function requerido(input) {
 }
 
 // // validar email
+
+function revisarEmail(input) {
+
+    let expresion = /\w+@\w+\.[a-z]/;
+
+    if (input.value != "" && expresion.test(input.value)) {
+        input.className = "form-control is-valid";
+        return true;
+    } else {
+        input.className = "form-control is-invalid";
+        return false;
+    }
+}
+
+
+function revisarConsulta(consulta) {
+    if (consulta.value.length >= 10) {
+        consulta.className = "form-control is-valid";
+        return true;
+    } else {
+        consulta.className = "form-control is-invalid";
+        return false;
+    }
+}
+
+/// validar email
 
 function revisarEmail(input) {
     // juandami1982@gmail.com para validar
@@ -37,35 +63,21 @@ function revisarConsulta(consulta) {
     }
 }
 
-// // agregar un evento a un objeto html
 
-let checkTerminos = document.getElementById('terminos');
-// agregar evento
-checkTerminos.addEventListener('change', revisarTerminos);
-
-function revisarTerminos() {
-    if (checkTerminos.checked) {
-        checkTerminos.className = "form-check-input is-valid";
-        return true;
-    } else {
-        checkTerminos.className = "form-check-input is-invalid";
-        return false;
-    }
-}
-
-// // boton enviar
+// boton enviar
 
 function validarGeneral(event) {
     event.preventDefault();
     console.log('Desde la funcion validar general' + event);
     if (requerido(document.getElementById('nombre')) &&
         revisarEmail(document.getElementById('email')) &&
+        
         revisarConsulta(document.getElementById('consulta')) &&
         revisarTerminos()) {
         enviarEmail();
         alert("El formulario esta listo para ser enviado");
     } else {
-        document.getElementById('msjEnvio').className = 'alert alert-danger  bg-danger  my-3';
+        document.getElementById('msjEnvio').className = 'alert alert-danger my-3';
         document.getElementById('msjEnvio').innerText = 'ocurrio un error';
     }
 }
@@ -74,7 +86,8 @@ function enviarEmail() {
     let template_params = {
         from_name: document.getElementById('nombre').value,
         to_name: "juan damichici",
-        message_html: `('email').value} - ${document.getElementById('consulta').value}`
+        message_html: `email: ${document.getElementById
+                ('email').value} - ${document.getElementById('consulta').value}`
     };
 
     let service_id = "default_service";
