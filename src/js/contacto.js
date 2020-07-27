091373876d329ed101c92185021b4a63fcf2a5fa
juan <juandami1982@gmail.com>
2020-07-27 10:03:01 -0300

    se agrego contacto.js

--- src/js/contacto.js
+++ src/js/contacto.js
@@ -1,3 +1,8 @@
+import "@fortawesome/fontawesome-free/js/all.js"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap";
+import "../css/style.css";
+
 function requerido(input) {
 
     if (input.value != "") {
