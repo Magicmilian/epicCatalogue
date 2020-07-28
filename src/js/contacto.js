06824a0814de7da577aef4d7b2c8187ef79b96fc
Juan Arias <arias.juan.jose@outlook.com>
2020-07-27 22:03:21 -0300

    Se agrego funcionalidad de envio de mail.

--- src/js/contacto.js
+++ src/js/contacto.js
@@ -46,41 +46,34 @@ window.revisarConsulta = function (consulta) {
 
 window.validarGeneral = function (event) {
     event.preventDefault();
-    console.log('Desde la funcion validar general' + event);
+    console.log('Desde la funcion validar general');
     if (requerido(document.getElementById('nombre')) &&
         revisarEmail(document.getElementById('email')) &&
-        
-        revisarConsulta(document.getElementById('consulta')) &&
-        revisarTerminos()) {
+        revisarConsulta(document.getElementById('consulta'))) {
         enviarEmail();
-        alert("El formulario esta listo para ser enviado");
     } else {
         document.getElementById('msjEnvio').className = 'alert alert-danger my-3';
-        document.getElementById('msjEnvio').innerText = 'ocurrio un error';
+        document.getElementById('msjEnvio').innerText = 'Debe completar todos los campos';
     }
 }
 
 window.enviarEmail = function () {
     let template_params = {
         from_name: document.getElementById('nombre').value,
-        to_name: "juan damichici",
+        to_name: "Juan Damichici",
         message_html: `email: ${document.getElementById
                 ('email').value} - ${document.getElementById('consulta').value}`
     };
-
     let service_id = "default_service";
     let template_id = "template_iT1uMq0N";
     emailjs.send(service_id, template_id, template_params).then(function (response) {
-        // esto se ejecuta si se envio correctamente el mail
         console.log(response);
         document.getElementById('msjEnvio').className = 'alert alert-primary my-3';
-        document.getElementById('msjEnvio').innerText = 'su consulta fue enviada.';
-
+        document.getElementById('msjEnvio').innerText = 'Su consulta fue enviada.';
     }, function (error) {
         console.log("error", error);
         document.getElementById('msjEnvio').className = 'alert alert-danger my-3';
         document.getElementById('msjEnvio').innerText = 'A ocurrido un error en el envio.';
-
     }
     );
 }
